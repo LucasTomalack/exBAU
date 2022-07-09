0bce771f7347f579b4e5913394bba52fe1c23ebd
Vinicius <vinioliveira.s@hotmail.com>
2022-07-09 12:36:14 -0300

    testing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 
 #include "exbau.hpp"
+#include <vector>
 
+using namespace std;
 
 int main(){
     FILE *disk = fopen("test.img", "rb+");    
@@ -10,7 +12,13 @@ int main(){
         return 0;
     }
 
-    format_disk(disk);
+    // format_disk(disk);
+    BootRecord boot_record = read_boot_record(disk);;
+    
+    copy_file(disk, boot_record, "copy.txt");
+    copy_file(disk, boot_record, "another.txt");
+    // copy_file(disk, boot_record, "bigfile.txt");
+    cout << find_offset_sector_data(0,512,boot_record.reserved_sectors) << endl;
     fclose(disk);
     return 0;
 }
